91aa82744473f78ce72332f6f9afc20668d3bff0
Danrodsf <90701015+Danrodsf@users.noreply.github.com>
2021-09-29 21:46:44 +0200

    añadida funcion para cambiar de fase

--- js/main.js
+++ js/main.js
@@ -1,24 +1,46 @@
 let Meta = 1000;
 
 class Coche {
-    constructor(name, maxspeed) {
+    constructor(name, speed, distanceToGoal) {
         this.name = name,
-            this.maxspeed = maxspeed
+            this.speed = speed,
+            this.distanceToGoal = distanceToGoal
     }
 
     Advance() {         //Funcion Avanzar
-        Meta -= Avanza;
+        this.distanceToGoal += random;
     }
 }
 
-let subaru = new Coche("Subaru", 200);
-let Toyota = new Coche("Toyota", 185);
+let subaru = new Coche("Subaru", 200, 0);
+let toyota = new Coche("Toyota", 185, 0);
 
 
-function Acelerar(eleccion) {           //Boton de acelerar
-    document.getElementById(eleccion)
-    subaru.Advance();
-    console.log(Meta)
+function Acelerar(car) {           //Boton de acelerar
+    car.Advance();
+    console.log(car.distanceToGoal);
 }
 
-let Avanza = Math.floor(Math.random() * (200 - 100) + 100) // Numero Azar entre 100 y 200
+function Acelerar2(car) {           //Boton de acelerar
+    car.Advance();
+    console.log(car.distanceToGoal);
+}
+
+let random = Math.floor(Math.random() * (200 - 100) + 100) // Numero Azar entre 100 y 200
+
+Acelerar(subaru);
+Acelerar2(toyota);
+
+organizer = (arg_O) => {
+
+    let fasewant = "fase" + arg_O;
+    let arrFases = ["fase1", "fase2", "fase3", "fase4", "fase5"];
+
+    arrFases = arrFases.filter(val => !fasewant.includes(val));
+
+    document.getElementById(fasewant).style.display = "block";
+
+    for (let _f of arrFases) {
+        document.getElementById(_f).style.display = "none";
+    }
+}
